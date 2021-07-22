fad1053c1341824aa81c3958d408fe357f8e4865
JohnPower71 <stkautoru@mail.ru>
2021-07-22 14:06:34 +0300

    add create, delete todos

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-alert("hello world")
+
+const todoBody = document.querySelector(".todo-wrapper")
+const createTodoButton = document.querySelector(".create-todo-button");
+const createTodoTitleField = document.querySelector(".title-input");
+const createTodoTextField = document.querySelector(".body-input");
+
+const createTodo = (titleText, text) => {
+    const newTodo = document.createElement("div");
+
+    newTodo.className += 'todo-item'
+
+    const id = document.createElement("div");
+    const title = document.createElement("div");
+    const body = document.createElement("div");
+    const deleteButton = document.createElement("button");
+
+    deleteButton.addEventListener('click', (ev) => {
+            todoBody.removeChild(newTodo)
+    })
+
+    id.innerHTML = Math.floor(Math.random() * 100)
+    title.innerHTML = titleText;
+    body.innerHTML = text;
+    deleteButton.innerHTML = "Remove"
+
+    newTodo.appendChild(id)
+    newTodo.appendChild(title)
+    newTodo.appendChild(body)
+    newTodo.appendChild(deleteButton)
+
+    todoBody.appendChild(newTodo)
+}
+
+
+createTodoButton.addEventListener('click', (ev) => {
+
+    const title = createTodoTitleField.value;
+    const text = createTodoTextField.value
+
+    createTodo(title, text)
+
+    createTodoTitleField.value = ""
+    createTodoTextField.value = ""
+})
